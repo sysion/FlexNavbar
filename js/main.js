8e7f17d5c96c31249a295ea25783d07066c1ae26
sysionng <sysionng@gmail.com>
2022-07-04 09:15:54 +0100

    Initial files and folder setup

---
+++ js/main.js
@@ -0,0 +1,43 @@
+;(()=>{
+	const toggle = document.querySelector('.toggle');
+	const top_nav = document.querySelector('.logo-nav > nav');
+	const projects_nav = document.querySelector('.projects-nav');
+	const projects = document.querySelector('.projects');
+	const project_sub_menu = document.querySelector('.project-sub-menu');
+	const project_sub_education = document.querySelector('.project-sub-education');
+	const project_sub_water = document.querySelector('.project-sub-water');
+	const project_sub_climate = document.querySelector('.project-sub-climate');
+
+	let toggleItem = (obj)=>{
+		if (obj.classList.contains('remove')){
+			obj.classList.remove('remove');
+			obj.classList.add('add');
+		}
+		else{
+			obj.classList.remove('add');
+			obj.classList.add('remove');	
+		}
+	}
+
+	let showMenu = ()=>{
+		/*if (top_nav.classList.contains('remove')){
+			top_nav.classList.remove('remove');
+			top_nav.classList.add('add');
+		}
+		else{
+			top_nav.classList.remove('add');
+			top_nav.classList.add('remove');	
+		}*/
+		toggleItem(top_nav);
+	};
+
+	let showSub = (e)=>{
+		//e.preventDefault();
+		toggleItem(projects_nav);
+	}
+
+	toggle.addEventListener('click', showMenu);
+	projects.addEventListener('click', showSub);
+
+
+})();
